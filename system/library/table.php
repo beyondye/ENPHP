19efcb48b64b39526a1a3a556dc7cb046ff6f833
Ding <ding@DingdeMacBook-Pro.local>
2018-01-14 20:47:34 +0800

    add

--- system/library/table.php
+++ system/library/table.php
@@ -7,32 +7,14 @@ namespace System\Library;
  * 
  * @author Ding <beyondye@gmail.com>
  */
-class Grid
+class Table
 {
 
     /**
      * 设置需要显示的字段
      * @var array
      */
-    public $field;
-
-    /**
-     * 设置需要显示的字段字面量标题
-     * @var array
-     */
-    public $title;
-
-    /**
-     * 设置需要隐藏的字段
-     * @var array
-     */
-    public $hide;
-
-    /**
-     * 设置需要转换显示的字段
-     * @var array
-     */
-    public $convert;
+    public $fields;
 
     /**
      * 设置需要过滤筛选字段表单项
@@ -44,25 +26,13 @@ class Grid
      * 设置操作表格需要的工具按钮
      * @var string
      */
-    public $tool;
-
-    /**
-     * 设置表格尾部额外字段
-     * @var array
-     */
-    public $after ;
-
-    /**
-     * 设置表格首部额外字段
-     * @var array
-     */
-    public $before;
+    public $tools;
 
     /**
      * 设置表格单项或多项操作
      * @var string
      */
-    public $operation;
+    public $operations;
 
     /**
      * 设置表格数据源
@@ -74,31 +44,10 @@ class Grid
      * 设置当前页码
      * @var int
      */
-    public $page_num = 0;
+    public $pager;
 
-    /**
-     * 设置当前分页地址 url?page=
-     * @var string
-     */
-    public $page_url = '';
+   
 
-    /**
-     * 设置每个页面条数
-     * @var int
-     */
-    public $page_size = 0;
-
-    /**
-     * 设置数据总条数
-     * @var int
-     */
-    public $total = 0;
-
-    /**
-     * 表格模板路径
-     * @var string
-     */
-    public $template = '';
 
     /**
      * 模板渲染返回html
