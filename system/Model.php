7aee8a74810679258e4e77ef1de6c392767cf4ee
Ding <beyondye@gmail.com>
2018-07-06 00:52:29 +0800

    update

--- system/Model.php
+++ system/Model.php
@@ -31,27 +31,6 @@ class Model extends \System\System
     public $schema = [];
 
     /**
-     * 验证的不合法字段
-     * 
-     * @var array
-     */
-    public $illegalFields = [];
-
-    /**
-     * 缺少的必要字段
-     * 
-     * @var array
-     */
-    public $incompleteFields = [];
-
-    /**
-     * 不是数据库的字段
-     * 
-     * @var array
-     */
-    public $notMemberFields = [];
-
-    /**
      * 读数据库名
      * 
      * @var string
@@ -66,26 +45,49 @@ class Model extends \System\System
     public $WDB = 'default';
 
     /**
-     * 构造函数
+     * 加载safe 字段验证
+     * 
+     * @param string $name
+     * @return object
      */
-    public function __construct()
+    public function __get($name)
     {
-        
+        parent::__get($name);
+
+        if ($name == 'safe') {
+            return $this->load($name, 'System','', $this->schema);
+        }
     }
 
+    /**
+     * 获取表全部记录
+     * 
+     * @return object array
+     */
     public function all()
     {
         return $this->select([]);
     }
 
+    /**
+     * 通过sql where条件获取数据
+     * 
+     * @param array $where
+     * @return object array
+     */
     public function where($where)
     {
         return $this->select(['where' => $where]);
     }
 
+    /**
+     * 获取最后插入的主键id
+     * 
+     * @return int
+     */
     public function lastid()
     {
-        return $this->db($this->RDB)->insert_id;
+        return $this->db($this->WDB)->insert_id;
     }
 
     /**
@@ -201,7 +203,7 @@ class Model extends \System\System
      * 
      * @return string
      */
-    public function schemaPrecode()
+    public function printSchema()
     {
 
         $data_type = [
@@ -241,113 +243,6 @@ class Model extends \System\System
         }
 
         return $content . '];';
-    }
-
-    /**
-     * 验证数据合法性，非法字段保存于$this->illegalFields
-     * 
-     * @param array $data 需要和schema key名一致
-     * 
-     * @return boolean
-     */
-    public function validate($data)
-    {
-        if (!$data) {
-            return false;
-        }
-
-        $pass = true;
-        foreach ($data as $key => $value) {
-
-            if (isset($this->schema[$key]['validate']) && $this->schema[$key]['validate']) {
-                if (is_array($value)) {
-                    foreach ($value as $rs) {
-                        if (preg_match("{$this->schema[$key]['validate']['regex']}", $rs) == 0) {
-                            $this->illegalFields[] = $key;
-                            $pass = false;
-                        }
-                    }
-                } else if (preg_match("{$this->schema[$key]['validate']['regex']}", $value) == 0) {
-                    $this->illegalFields[] = $key;
-                    $pass = false;
-                }
-            }
-        }
-
-        return $pass;
-    }
-
-    /**
-     * 验证是否缺少必要字段，缺少的必要字段保存于$this->incompleteFields
-     * 
-     * @param array $data 比较数据
-     * 
-     * @return boolean
-     */
-    public function complete($data)
-    {
-        if (!is_array($data)) {
-            return false;
-        }
-
-        $required = [];
-        foreach ($this->schema as $key => $value) {
-            if (isset($value['required']) && $value['required'] == true) {
-                $required[] = $key;
-            }
-        }
-
-        $pass = true;
-        foreach ($required as $rs) {
-            if (!isset($data[$rs])) {
-                $this->incompleteFields[] = $rs;
-                $pass = false;
-            }
-        }
-
-        return $pass;
-    }
-
-    /**
-     * 与schema默认数据合并，并且清理不存在于schema里面的字段，不是成员的字段保存于$this->notMemberFields
-     * 
-     * @param array $data 并入schema的数据
-     * @param array $without 不需要的字段
-     * 
-     * @return array 与schema默认合并后的数据
-     */
-    public function merge($data, $without = [])
-    {
-
-        if (!is_array($data)) {
-            return false;
-        }
-
-        $fields = [];
-        foreach ($this->schema as $key => $value) {
-            if (isset($value['default'])) {
-                $fields[$key] = $value['default'];
-            }
-        }
-
-        $given = [];
-        foreach ($data as $key => $value) {
-            if (isset($this->schema[$key])) {
-                $given[$key] = $value;
-            } else {
-                $this->notMemberFields[] = $key;
-            }
-        }
-
-        $merge = array_merge($fields, $given);
-
-        foreach ($without as $rs) {
-            if (isset($merge[$rs])) {
-                unset($merge[$rs]);
-            }
-        }
-
-        return $merge;
     }
 
 }
