42712c5bf2e4c1d68ee177c90f2c2c036006af23
ding <beyondye@gmail.com>
2019-05-07 15:04:10 +0800

    adjust files

--- application/config/test/database.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-//默认数据库
-    'default' => ['driver' => 'mysqli', 'host' => 'set.database.to.hosts.file', 'username' => 'root', 'password' => '123456', 'database' => 'car', 'port' => 3306, 'charset' => 'utf8'],
-];
