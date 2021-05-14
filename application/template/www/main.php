c71497c0ebe9f8958dcdd38ffadaedcd724ea9d6
ding <beyondye@gmail.com>
2021-05-14 18:33:18 +0800

    important upgrade

--- application/template/www/main.php
+++ application/template/www/main.php
@@ -1,12 +1,20 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title>测试页面</title>
-    </head>
-    <body>
-        <?php
-        echo $hello_world;
-        ?>
-    </body>
+<head>
+    <meta charset="UTF-8">
+    <title>测试页面</title>
+</head>
+<body>
+<header>
+    <?php self::view('header'); ?>
+</header>
+
+<?php
+echo $hello_world;
+?>
+
+<footer>
+    <?php self::view('footer'); ?>
+</footer>
+</body>
 </html>
