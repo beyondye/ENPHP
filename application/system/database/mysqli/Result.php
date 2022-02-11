dcce971c337fb46f48730a552b30810deda7c650
ding <beyondye@gmail.com>
2022-02-11 17:01:50 +0800

    Changes

--- application/system/database/mysqli/Result.php
+++ application/system/database/mysqli/Result.php
@@ -7,21 +7,18 @@ class Result
 
     /**
      * 数据集合大小
-     *
      * @var int
      */
-    public $num_rows = 0;
+    public int $num_rows = 0;
 
     /**
      * 查询结果对象实例
-     *
      * @var object
      */
-    public $result = null;
+    public object $result;
 
     /**
      * 构造函数
-     *
      * @param object $result 查询结果对象实例
      */
     public function __construct($result)
@@ -32,9 +29,7 @@ class Result
 
     /**
      * 返回数据集
-     *
      * @param string $type 返回结果类型
-     *
      * @return array 数据集合
      */
     public function result(string $type = 'object')
@@ -57,11 +52,9 @@ class Result
 
     /**
      * 返回数据某一条数据
-     *
      * @param int $n 集合数组标
      * @param string $type 返回类型
-     *
-     * @return array or object
+     * @return array|object|null
      */
     public function row(int $n = 0, string $type = 'object')
     {
