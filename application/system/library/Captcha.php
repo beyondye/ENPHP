a8803befed8906b6eae39dcb6926951519f69e8f
Ding <beyondye@gmail.com>
2026-05-20 15:53:48 +0800

    优化Captcha

--- application/system/library/Captcha.php
+++ application/system/library/Captcha.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace system\library;
 
 class Captcha
@@ -9,108 +11,110 @@ class Captcha
      *
      * @var string
      */
-    public $fontPath;
+    public string $fontPath;
 
     /**
      * 图片数据
      *
-     * @var string
+     * @var \GdImage
      */
-    private $image;
+    private \GdImage $image;
 
     /**
      * 生成几位验证码
      *
      * @var int
      */
-    public $charLen = 4;
+    public int $charLen = 4;
 
     /**
      * 验证码字符
      *
      * @var array
      */
-    private $arrChr = array();
+    private array $arrChr = array();
 
     /**
      * 图宽片
      *
      * @var int
      */
-    public $width = 100;
+    public int $width = 100;
 
     /**
      * 图片高
      *
      * @var int
      */
-    public $height = 30;
+    public int $height = 30;
 
     /**
      * 验证码文本
      *
      * @var string
      */
-    private $text = '';
+    private string $text = '';
 
     /**
      * 背景色
      *
      * @var string
      */
-    public $bgcolor = "#ffffff";
+    public string $bgcolor = "#ffffff";
 
     /**
      * 生成杂点
      *
      * @var bool
      */
-    private $showNoisePix = true;
+    private bool $showNoisePix = true;
 
     /**
      * 生成杂点数量
      *
      * @var int
      */
-    private $noiseNumPix = 80;
+    private int $noiseNumPix = 80;
 
     /**
      * 生成杂线
      *
      * @var bool
      */
-    private $showNoiseLine = true;
+    private bool $showNoiseLine = true;
 
     /**
      * 生成杂线数量
      *
      * @var int
      */
-    private $noiseNumLine = 2;
+    private int $noiseNumLine = 2;
 
     /**
      * 边框，当杂点、线一起作用的时候，边框容易受干扰
      *
      * @var bool
      */
-    public $showBorder = true;
+    public bool $showBorder = false;
 
     /**
      * 边框颜色
      *
      * @var string
      */
-    public $borderColor = "#cccccc";
+    public string $borderColor = "#cccccc";
 
     /**
      * 构造函数
      *
      * Captcha constructor.
      */
-    public function __construct()
+    public function __construct(string $fontPath = '')
     {
-        if (defined('APP_DIR')) {
-            $this->fontPath = APP_DIR . 'font/';
+        if ($fontPath) {
+            $this->fontPath = $fontPath;
+        } else {
+            $this->fontPath = SYS_DIR . 'fonts/';
         }
 
         $this->arrChr = array_merge(range(1, 9), range('A', 'Z'), range('a', 'z'));
@@ -119,20 +123,20 @@ class Captcha
     /**
      * 获取颜色
      *
-     * @param $color
+     * @param string $color
      *
-     * @return int|string|string[]|null
+     * @return int|false
      */
-    private function getColor($color)
+    private function getColor(string $color): int|false
     {
         $color = preg_replace("/^#/i", "", $color);
         $r = $color[0] . $color[1];
         $r = hexdec($r);
-        $b = $color[2] . $color[3];
-        $b = hexdec($b);
-        $g = $color[4] . $color[5];
+        $g = $color[2] . $color[3];
         $g = hexdec($g);
-        $color = imagecolorallocate($this->image, $r, $b, $g);
+        $b = $color[4] . $color[5];
+        $b = hexdec($b);
+        $color = imagecolorallocate($this->image, $r, $g, $b);
 
         return $color;
     }
@@ -142,7 +146,7 @@ class Captcha
      *
      * @return void
      */
-    private function setNoisePix()
+    private function setNoisePix(): void
     {
         for ($i = 0; $i < $this->noiseNumPix; $i++) {
             $randColor = imageColorAllocate($this->image, rand(0, 255), rand(0, 255), rand(0, 255));
@@ -155,7 +159,7 @@ class Captcha
      *
      * @return void
      */
-    private function setNoiseLine()
+    private function setNoiseLine(): void
     {
         for ($i = 0; $i < $this->noiseNumLine; $i++) {
             $randColor = imageColorAllocate($this->image, rand(0, 255), rand(0, 255), rand(0, 255));
@@ -168,11 +172,11 @@ class Captcha
      *
      * @return void
      */
-    public function create()
+    public function create(): void
     {
         $this->image = imageCreate($this->width, $this->height);
-        $this->back = $this->getColor($this->bgcolor);
-        imageFilledRectangle($this->image, 0, 0, $this->width, $this->height, $this->back);
+        $backgroundColor = $this->getColor($this->bgcolor);
+        imageFilledRectangle($this->image, 0, 0, $this->width, $this->height, $backgroundColor);
 
         $size = $this->width / $this->charLen - 4;
         if ($size > $this->height) {
@@ -183,13 +187,13 @@ class Captcha
         $code = '';
         for ($i = 0; $i < $this->charLen; $i++) {
             $randKey = rand(0, count($this->arrChr) - 1);
-            $randText = $this->arrChr[$randKey];
+            $randText = (string) $this->arrChr[$randKey];
             $code .= $randText;
             $textColor = imageColorAllocate($this->image, rand(0, 100), rand(0, 100), rand(0, 100));
             $font = $this->fontPath . '/' . rand(1, 5) . ".ttf";
-            $randsize = rand($size - $size / 10, $size + $size / 10);
-            $location = $left + ($i * $size + $size / 10);
-            imagettftext($this->image, $randsize, rand(-18, 18), $location, rand($size - $size / 10, $size + $size / 10) + 2, $textColor, $font, $randText);
+            $randsize = rand(intval($size - $size / 10), intval($size + $size / 10));
+            $location = intval($left) + intval($i * $size + $size / 10);
+            imagettftext($this->image, $randsize, rand(-18, 18), $location, rand(intval($size - $size / 10), intval($size + $size / 10)) + 2, $textColor, $font, $randText);
         }
 
         if ($this->showNoisePix == true) {
@@ -201,8 +205,8 @@ class Captcha
         }
 
         if ($this->showBorder == true) {
-            $this->borderColor = $this->getColor($this->borderColor);
-            imageRectangle($this->image, 0, 0, $this->width - 1, $this->height - 1, $this->borderColor);
+            $borderColor = $this->getColor($this->borderColor);
+            imageRectangle($this->image, 0, 0, $this->width - 1, $this->height - 1, $borderColor);
         }
 
         $this->text = strtolower($code);
@@ -213,11 +217,10 @@ class Captcha
      *
      * @return void
      */
-    public function show()
+    public function show(): void
     {
         header("Content-type: image/png");
         imagepng($this->image);
-        imagedestroy($this->image);
     }
 
     /**
@@ -225,9 +228,8 @@ class Captcha
      *
      * @return string
      */
-    public function getCode()
+    public function getCode(): string
     {
         return $this->text;
     }
-
 }
